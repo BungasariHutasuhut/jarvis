460d3f75ee29d0e34b4eca326fa7c713bc846a97
bungahts15@gmail.com <bungahts15@gmail.com>
2023-03-03 21:08:49 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+//middleware =>funtion yang me
+app.use((req, res, next) => {
+  if (req.url === "/sari") {
+    console.log("Sari");
+  }
+  console.log(req.url);
+  next();
+});
+
+//route
+app.get("/", (req, res) => {
+  res.send("Sari");
+});
+
+app.listen(3000, () => {
+  console.log("Berhasil !!");
+});
